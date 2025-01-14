9b052ab33a6905d5b4f0564f719564b38548ce14
Infamous003 <arfathsyed0312@gmail.com>
2025-01-14 12:34:56 +0530

    Add POST request for creating books

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, status
-from models import Book
+from models import Book, BookCreate
 from database import create_db_and_tables, db
 from sqlmodel import Session, select
 
@@ -19,8 +19,6 @@ def get_books() -> list[Book]:
 
 @app.get("/books/{id}")
 def get_books(id: int) -> Book:
-  bookExists = [book for book in BOOKS if book.get("id") == id]
-
   with Session(db) as session:
     statement = select(Book).where(Book.id == id)
     bookExists = session.exec(statement).one_or_none()
@@ -30,8 +28,31 @@ def get_books(id: int) -> Book:
     return bookExists
 
 
-# @app.post("/books")
-# def create_books(book: Book):
-#   with(Session) as session:
-#     statement = select(Book)
+@app.post("/books")
+def create_books(book: BookCreate):
+  with Session(db) as session:
+    statement = select(Book).where(Book.title == book.title)
+    bookExists = session.exec(statement).one_or_none()
+    
+    if bookExists:
+      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book already exists")
+    
+    new_book = Book(**book.model_dump())
 
+    session.add(new_book)
+    session.commit()
+    session.refresh(new_book)
+    return new_book
+
+
+
+def addBooks():
+  with Session(db) as session:
+    b1 = Book(title="ksgbjskd", author="okkok", pages=123)
+    b2 = Book(title="ksgbjssdsdskd", author="fvokkok", pages=1323)
+    b3 = Book(title="ksgbsddsjskd", author="okvfvkok", pages=1553)
+
+    session.add_all([b1, b2, b3])
+    session.commit()
+
+# addBooks()
--- models.py
+++ models.py
@@ -5,5 +5,9 @@ class Book(SQLModel, table = True):
   id: int = Field(primary_key=True, nullable=False)
   title: str = Field(String(128), nullable=False, unique=True)
   author: str = Field(String(64), nullable=False)
-  pages: int = Field(Integer())
-  
+  pages: int | None = Field(default=None)
+
+class BookCreate(BaseModel):
+  title: str
+  author: str
+  pages: int | None = None
